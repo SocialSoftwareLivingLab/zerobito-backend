1d463f2ff5de28c3541027770b87cc9c0cda571e
Gabriel Ferreira <64704484+Harlock221B@users.noreply.github.com>
2023-12-06 17:36:17 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,11 @@ import { routes } from "./routes";
 
 const app = express();
 
+// Configure o CORS antes de qualquer outra middleware
 app.use(cors({
+    origin: 'http://143.106.73.48:3000', // Substitua pelo domínio do seu site
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Se você precisa de suporte a credenciais (cookies, autenticação)
 }));
 
 app.use(express.json());
