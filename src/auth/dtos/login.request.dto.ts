0631bbb0a9cba8a9e67079bbca93b4faf4bd2eaa
Henrique P <102121798+HenriquePManoel@users.noreply.github.com>
2024-04-08 16:26:27 -0300

    Update login.request.dto.ts
    desfazendo alterações

--- src/auth/dtos/login.request.dto.ts
+++ src/auth/dtos/login.request.dto.ts
@@ -5,7 +5,6 @@ export class LoginDto {
     description: 'E-mail do usuário',
     example: 'fulano@email.com',
   })
-  @IsEmail({}, { message: 'E-mail inválido' })
   email: string;
 
   @ApiProperty({ description: 'Senha do usuário', example: 'Senh@Dificil1*' })
