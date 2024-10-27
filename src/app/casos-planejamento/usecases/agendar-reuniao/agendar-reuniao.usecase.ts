f5c583566dc90ced6a6f60a2eb7acde02efdd05d
Leonardo Braz <lhleonardo05@gmail.com>
2024-10-27 14:18:29 -0300

    feat: adiciona endpoint para marcar reunião

--- src/app/casos-planejamento/usecases/agendar-reuniao/agendar-reuniao.usecase.ts
+++ src/app/casos-planejamento/usecases/agendar-reuniao/agendar-reuniao.usecase.ts
@@ -5,9 +5,11 @@ import AgendamentoReuniaoEntity from "../../entities/agendamento-reuniao.entity"
 import { InjectRepository } from "@nestjs/typeorm";
 import AppException from "@/shared/exceptions/app-exception";
 import { MensagensHelper } from "@/helpers/mensagens.helper";
+import ConsultarCasoPorIdUsecase from "@/app/casos/usecases/caso/consultar-casos/consultar-caso-by-id.usecase";
+import { isAfter } from "date-fns";
 
 export interface AgendarReuniaoUsecaseRequest {
-  data: Date;
+  dataAgendamento: Date;
   idCaso: number;
   solicitante: UsuarioAutenticadoDto;
 }
@@ -17,14 +19,27 @@ export default class AgendarReuniaoUsecase {
 
   constructor(
     @InjectRepository(AgendamentoReuniaoEntity)
-    private readonly agendamentoReuniaoRepository: Repository<AgendamentoReuniaoEntity>
+    private readonly agendamentoReuniaoRepository: Repository<AgendamentoReuniaoEntity>,
+    private readonly consultarCasoByIdUsecase: ConsultarCasoPorIdUsecase,
   ) { }
 
-  public async executar({ data, idCaso, solicitante }: AgendarReuniaoUsecaseRequest): Promise<void> {
+  public async executar({ dataAgendamento, idCaso, solicitante }: AgendarReuniaoUsecaseRequest): Promise<void> {
+
+    const casoInformadoExiste = await this.consultarCasoByIdUsecase.casoExiste(idCaso);
+
+    if (!casoInformadoExiste) {
+      throw new AppException(MensagensHelper.Casos.CASO_NAO_ENCONTRADO);
+    }
+
+    const isDataAgendamentoValida = isAfter(dataAgendamento, new Date());
+
+    if (!isDataAgendamentoValida) {
+      throw new AppException(MensagensHelper.CasosAgendamentoReuniaoPreparacao.HORARIO_INVALIDO_PARA_AGENDAMENTO);
+    }
 
     const existeAgendamentoJaMarcado = await this.agendamentoReuniaoRepository.find({
       where: {
-        data,
+        data: dataAgendamento,
         caso: {
           id: idCaso
         },
@@ -36,7 +51,7 @@ export default class AgendarReuniaoUsecase {
     }
 
     const agendamentoParaSalvar = this.agendamentoReuniaoRepository.create({
-      data,
+      data: dataAgendamento,
       caso: { id: idCaso },
       solicitante: {
         id: solicitante.id
